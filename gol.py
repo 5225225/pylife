d7b7c185662594636271e687a4c66e6651204b11
5225225 <mrhouse991@gmail.com>
2014-01-12 20:34:41 +0000

    Changes

--- gol.py
+++ gol.py
@@ -3,12 +3,21 @@ import random
 import time
 
 def near(boarditem):
-    sitem = boarditem.split(":")
+    sitem = boarditem.partition(":")
     x = int(sitem[0])
-    y = int(sitem[1])
+    y = int(sitem[2])
     found = 0
+    #This is the slow bit, as far as I can tell.
+    #This is the quickest way of doing it, though.
+    if "{}:{}".format(x-1,y-1) in board: found += 1 
+    if "{}:{}".format(x-1,y) in board: found += 1
+    if "{}:{}".format(x-1,y+1) in board: found += 1
+    if "{}:{}".format(x,y-1) in board: found += 1
+    if "{}:{}".format(x,y+1) in board: found += 1
+    if "{}:{}".format(x+1,y-1) in board: found += 1
+    if "{}:{}".format(x+1,y) in board: found += 1
+    if "{}:{}".format(x+1,y+1) in board: found += 1
 
-    
     return found
 
 
@@ -23,21 +32,17 @@ ywidth = 170
 stay = (2,3)
 born = (3,)
 
+FPS = 99
+
 for x in range(0,xwidth):
     for y in range(0,ywidth):
         if random.random() < chance:
             board.add("{}:{}".format(x,y))
 tick = 0
 starttime = time.time()
+lastprint = 0
 
 outstr = ""
-for x in range(0,xwidth):
-    for y in range(0,ywidth):
-        if "{}:{}".format(x,y) in board:
-            outstr += "#"
-        else:
-            outstr += "."
-    outstr += "\n"
 
 while True:
     outstr = ""
@@ -48,7 +53,6 @@ while True:
             xystr = "{}:{}".format(x,y)
             
             nearnum = near(xystr)
-            
             if xystr in board:
                 if nearnum not in stay:
                     newboard.discard(xystr)
@@ -57,17 +61,21 @@ while True:
                     newboard.add(xystr)
 
     board = newboard.copy()
-    for x in range(0,xwidth):
-        for y in range(0,ywidth):
-            if "{}:{}".format(x,y) in board:
-                outstr += "#"
-            else:
-                outstr += "."
-        outstr += "\n"
-    print("\n"*99)
-    sys.stdout.write(outstr)
-    sys.stdout.flush()
-    print("""
-POPULATION: {}
-TICK:       {}
-TPS:        {}""".format(len(board), tick, round(tick / (time.time() - starttime), 2)) )
+    
+    if time.time() - lastprint > 1/FPS :
+        lastprint = time.time()
+        for x in range(0,xwidth):
+            for y in range(0,ywidth):
+                if "{}:{}".format(x,y) in board:
+                    outstr += "#"
+                else:
+                    outstr += "."
+            outstr += "\n"
+        print("\n"*99)
+        sys.stdout.write(outstr)
+        sys.stdout.flush()
+        print("""
+    POPULATION: {}
+    TICK: {}
+    TPS: {}""".format(len(board), tick, round(tick / (time.time() - starttime), 2)) )
+
