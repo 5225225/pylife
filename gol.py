ea3657c3bfbcc2f1987e77ec619785330672062a
5225225 <mrhouse991@gmail.com>
2014-01-13 08:16:53 +0000

    PEP8 (and some other fixes that I didn't commit, like changing to ints)

--- gol.py
+++ gol.py
@@ -2,42 +2,48 @@ import sys
 import random
 import time
 
+
 def near(boarditem):
-    sitem = boarditem.partition(":")
-    x = int(sitem[0])
-    y = int(sitem[2])
+    x = int(boarditem[0])
+    y = int(boarditem[1])
     found = 0
     #This is the slow bit, as far as I can tell.
     #This is the quickest way of doing it, though.
-    if "{}:{}".format(x-1,y-1) in board: found += 1 
-    if "{}:{}".format(x-1,y) in board: found += 1
-    if "{}:{}".format(x-1,y+1) in board: found += 1
-    if "{}:{}".format(x,y-1) in board: found += 1
-    if "{}:{}".format(x,y+1) in board: found += 1
-    if "{}:{}".format(x+1,y-1) in board: found += 1
-    if "{}:{}".format(x+1,y) in board: found += 1
-    if "{}:{}".format(x+1,y+1) in board: found += 1
-
+    if (x-1, y-1) in board:
+        found += 1
+    if (x-1, y) in board:
+        found += 1
+    if (x-1, y+1) in board:
+        found += 1
+    if (x, y-1) in board:
+        found += 1
+    if (x, y+1) in board:
+        found += 1
+    if (x+1, y-1) in board:
+        found += 1
+    if (x+1, y) in board:
+        found += 1
+    if (x+1, y+1) in board:
+        found += 1
     return found
-
 
 
 board = set()
 
 chance = .25
 
-xwidth = 40
-ywidth = 170
+xwidth = 100
+ywidth = 100
 
-stay = (2,3)
-born = (3,)
+stay = (2, 3)
+born = (3, )
 
-FPS = 99
+FPS = 100
 
-for x in range(0,xwidth):
-    for y in range(0,ywidth):
+for x in range(0, xwidth):
+    for y in range(0, ywidth):
         if random.random() < chance:
-            board.add("{}:{}".format(x,y))
+            board.add((x, y))
 tick = 0
 starttime = time.time()
 lastprint = 0
@@ -48,34 +54,33 @@ while True:
     outstr = ""
     tick += 1
     newboard = board.copy()
-    for x in range(0,xwidth):
-        for y in range(0,ywidth):
-            xystr = "{}:{}".format(x,y)
-            
-            nearnum = near(xystr)
-            if xystr in board:
+    for x in range(0, xwidth):
+        for y in range(0, ywidth):
+            nearnum = near((x, y))
+            if (x, y) in board:
                 if nearnum not in stay:
-                    newboard.discard(xystr)
+                    newboard.discard((x, y))
             else:
                 if nearnum in born:
-                    newboard.add(xystr)
+                    newboard.add((x, y))
 
     board = newboard.copy()
-    
-    if time.time() - lastprint > 1/FPS :
+
+    if time.time() - lastprint > 1/FPS:
         lastprint = time.time()
-        for x in range(0,xwidth):
-            for y in range(0,ywidth):
-                if "{}:{}".format(x,y) in board:
+        for x in range(0, xwidth):
+            for y in range(0, ywidth):
+                if (x, y) in board:
                     outstr += "#"
                 else:
                     outstr += "."
             outstr += "\n"
-        print("\n"*99)
         sys.stdout.write(outstr)
         sys.stdout.flush()
         print("""
     POPULATION: {}
     TICK: {}
-    TPS: {}""".format(len(board), tick, round(tick / (time.time() - starttime), 2)) )
-
+    TPS: {}""".format(
+            len(board),
+            tick,
+            round(tick / (time.time() - starttime), 5)))
